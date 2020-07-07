218ab5507557a734d068cce49ecd4cb1332a0389
Ivan Cheng <ivan@ivancheng.co.uk>
2020-07-07 11:26:53 +0100

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -33,12 +33,10 @@ module.exports = {
         }),
         new WorkboxPlugin.GenerateSW(),
         new webpack.DefinePlugin({
-            PRODUCTION: JSON.stringify(true),
-            VERSION: JSON.stringify('5fa3b9'),
-            BROWSER_SUPPORTS_HTML5: true,
-            TWO: '1+1',
-            'typeof window': JSON.stringify('object'),
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                PORT: JSON.stringify(process.env.PORT)
+            },
         })
     ]
 }
